0993f1d3d98b4b538dd8edf73f9f2a401b10a348
jmlinares2019 <jlinaresmar@gmail.com>
2021-12-26 18:29:58 +0100

    Cross-component events (downwards & upwards) / filtering data with pipes

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { without } from 'lodash';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +11,16 @@ export class AppComponent implements OnInit{
   title = 'Wisdom Pets Medicine';
   theList: object[];
 
+  deleteApt(theApt: object){
+    this.theList = without(this.theList, theApt);
+  }
+
   constructor( private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get<Object[]>('../assets/data.json').subscribe(data => {
       this.theList = data;
+      console.log(data);
     });
   }
 
